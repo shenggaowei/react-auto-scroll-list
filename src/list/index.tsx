5ef30b2e8940a68162210fc544ab8e0b37b4db50
gaogao <weishenggao@innobuddy.com>
2021-11-19 17:45:55 +0800

    chore: build方式修改为库模式

---
+++ src/list/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactAutoScrollList: React.FC = () => {
+  return <div>啊哈哈</div>;
+};
+
+export default ReactAutoScrollList;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/list/index.tsx"),
+      name: "ReactAutoScrollList",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
